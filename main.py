db1ea0aa9fd96891c087163d83c5e3266a81cc65
Prathesha <prathesha.m27@gmail.com>
2023-10-08 13:12:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+@app.route('/')
+def welcome():
+     return render_template('index.html')
+
+
+@app.route('/predict', methods=('GET', 'POST'))
+def predict():
+     title = request.form['title']
+     return title
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
